2ca4d0423e4d87a190c8a5f95086c659f17d5db0
BRinvelt <rinvelt@wisc.edu>
2021-07-09 09:47:26 -0400

    Added branch option and example awards
    Added an optional branch name CLA to support custom branches or newer repos.
    
    Added a few simple example User Awards --aw

--- gitStatistics.py
+++ gitStatistics.py
@@ -15,10 +15,10 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 queryBase = """
-query($repo: String!, $owner: String!)
+query($repo: String!, $owner: String!, $branch: String!)
 {
     repository(name: $repo, owner: $owner){
-    ref(qualifiedName: "master") {
+    ref(qualifiedName: $branch) {
       target {
         ... on Commit {
           id
@@ -51,6 +51,8 @@ class gitStatistics:
         #Initialize argument parser
         self.argParser = self.__initArgParser()
         self.args = self.argParser.parse_args(args)
+        if not self.args.branch:
+            self.args.branch = "master"
         #Create a session to set default call Header information
         self.session = requests.session()
         self.session.headers = {"Authorization":"token " + self.args.apiKey}
@@ -71,12 +73,14 @@ class gitStatistics:
         argParser = argparse.ArgumentParser(description='Gathers statistics on activity in targetted GitHub Repositories, default selection are repositories within current directory')
         argParser.add_argument("apiKey", help = 'Github api key')
         argParser.add_argument("owners", help = 'Comma separated Owner(s) of targeted Repos')
+        argParser.add_argument("--branch","--b", help = 'User defined target branch, default is master')
         argParser.add_argument("--hostname", "--hn", help = 'https://<user specified hostname>/api/graphql - use for enterprise')
         argParser.add_argument("--repos","--r", help = 'Comma separated repo(s) to analyze')
         argParser.add_argument("--addr", help = 'Address of a folder containing repos or csv containing repo names')
         argParser.add_argument("--wordCloud","--wc", help = 'Generates word clouds from commit messages', action = 'store_true')
         argParser.add_argument("--graphStats","--gs", help = 'Displays Simple Graphs of data', action = 'store_true')
         argParser.add_argument("--csv", help = 'Stores recorded data as a CSV', action = 'store_true')
+        argParser.add_argument("--awards","--aw", help = 'Prints awards/titles for users based on their statistics', action = 'store_true')
         return argParser
 
     def makeWordCloud(self):
@@ -124,12 +128,51 @@ class gitStatistics:
                 outputWriter.writerow(row)
 
     def grantAwards(self):
-        ##Award List
-        #Most Sporadic Committer
-        #Prolific Puller
-        #Weekend Warrior
-        #
-        return
+        committedUser = None
+        committedScore = None
+        committedDate = None
+        weekendUser = None
+        weekendScore = None
+        additionUser = None
+        additionScore = None
+        additionCommits = None
+        for user in self.users:
+            userStats = self.users.get(user)
+            #Weekend Warrior
+            userScore = userStats[5][5] + userStats[5][6]
+            if weekendUser is not None:
+                if userScore > weekendScore:
+                    weekendUser = user
+                    weekendScore = userScore
+            else:
+                weekendUser = user
+                weekendScore = userScore
+            #Committed Committer
+            userScore =  userStats[2]
+            if committedUser is not None:
+                if userScore > committedScore:
+                    committedUser = user
+                    committedScore = userScore
+                    committedDate = userStats[4]
+            else:
+                committedUser = user
+                committedScore = userScore
+                committedDate = userStats[4]
+            #Heavy Hitter
+            userScore = userStats[0] / userStats[2]
+            if additionUser is not None:
+                if userScore > additionScore:
+                    additionUser = user
+                    additionScore = userScore
+                    additionCommits = userStats[2]
+            else:
+                additionUser = user
+                additionScore = userScore
+                additionCommits = userStats[2]
+
+        print(weekendUser,"is the WEEKEND WARRIOR with",weekendScore,"commits logged on weekends")
+        print(committedUser,"is the COMMITTED COMMITTER with",committedScore,"commits logged since joining on",committedDate[1],"/",committedDate[2],"of",committedDate[0])
+        print("Watch out for this HEAVY HITTER...",additionUser,"has averaged",additionScore,"additions over",additionCommits,"commits")
 
     def graphStats(self):
         additionBar=[] 
@@ -201,17 +244,19 @@ class gitStatistics:
 
         for repo in repos:
             for owner in owners:
-
-                params = {"repo":repo,"owner":owner}
+                params = {"repo":repo,"owner":owner, 'branch':self.args.branch}
                 response = self.session.post(self.url,json={'query':queryBase, 'variables':params})
                 data = json.loads(response.text)
                 data = data["data"]["repository"]
 
                 if not data: #if the owner/repository combination is invalid, conitnue to next
                     continue
+                data = data["ref"]
+                if not data:
+                    continue
 
                 #parse data from response for each commit
-                data = data["ref"]["target"]["history"]["edges"]
+                data = data["target"]["history"]["edges"]
                 for commit in data:
                     commit = commit['node']
                     author = commit["author"]["name"]
@@ -241,15 +286,23 @@ class gitStatistics:
                         weekendCommit = user[5]
                         weekendCommit[weekdayCheck]+=1
                         startDate = date
+                        print (author)
+                        print(startDate)
+                        print(divmod((datetime.date(int(user[4][0]),int(user[4][1]),int(user[4][2]))-datetime.date(int(date[0]),int(date[1]),int(date[2]))).total_seconds()  ,86400)[0])
                         self.users[author] = [additions, deletions,commits,commitMessage,date, weekdayCommitCount]
+
     def execute(self):
         self.getStats()
-        if self.args.wordCloud:
-            self.makeWordCloud()
-        if self.args.graphStats:
-            self.graphStats()
-        if self.args.csv:
-            self.makeCSV()
+        if self.users != {}:
+            if self.args.wordCloud:
+                self.makeWordCloud()
+            if self.args.graphStats:
+                self.graphStats()
+            if self.args.csv:
+                self.makeCSV()
+            if self.args.awards:
+                self.grantAwards()
+
 if __name__ == "__main__":
     engine = gitStatistics(sys.argv[1:])
     engine.execute()
